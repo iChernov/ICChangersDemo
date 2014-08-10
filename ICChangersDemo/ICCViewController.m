32592373ac0afcb075c258b23a2e9a82cbd1b69a
Ivan Chernov <ichernov@me.com>
2014-08-10 22:55:30 +0200

    Downloading and parsing JSON from the server
    included cocoapod AFNetworking for easier download processing

--- ICChangersDemo/ICCViewController.m
+++ ICChangersDemo/ICCViewController.m
@@ -7,6 +7,15 @@
 //
 
 #import "ICCViewController.h"
+#import "AFNetworking.h"
+
+static NSString * const BaseURLString = @"http://comm.new.changers.com/services/api/rest/json/";
+
+static NSString * const kCO2EmissionKey = @"co2avoidedTotal";
+static NSString * const kMainFootPrintKey = @"indexCurrent";
+static NSString * const kLastMonthFootPrintKey = @"indexPrevMonth";
+static NSString * const kLastYearFootprintKey = @"indexPrevYear";
+
 
 @interface ICCViewController ()
 @property IBOutlet UIView *contentView;
@@ -17,10 +26,9 @@
 @property IBOutlet UILabel *kilometerLabel;
 @property IBOutlet UILabel *emissionLabel;
 
-@property IBOutlet UIView *arrowView;
-@property IBOutlet UIView *redRotatingBallView;
-@property IBOutlet UIView *redRotatingPointerView;
-
+@property IBOutlet UIImageView *arrowView;
+@property IBOutlet UIImageView *redRotatingBallView;
+@property IBOutlet UIImageView *redRotatingPointerView;
 @end
 
 @implementation ICCViewController
@@ -31,8 +39,71 @@
     self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName: [UIColor whiteColor]};
     self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
     self.navigationController.navigationBar.topItem.title = @"";
-
+    [self setInitialValues];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)setInitialValues {
+    _arrowView.image = nil;
+    _pastDaysFootprintLabel.text = @"LOADING FOOTPRINT...";
+    _mainFootprintLabel.text = @"--";
+    _kilometerLabel.text = @"--";
+    _emissionLabel.text = @"--";
+
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self loadData];
+}
+
+- (void)loadData {
+    NSString *methodParameterString = @"method=statistics.user";
+    NSString *apiKeyParameterString = @"api_key=87718188734753b091a72b68571c2a478a8d6961";
+    NSString *usernameParameterString = @"username=chardaker";
+    NSString *string = [NSString stringWithFormat:@"%@?%@&%@&%@",
+                        BaseURLString,
+                        methodParameterString,
+                        apiKeyParameterString,
+                        usernameParameterString];
+    NSLog(@"%@", string);
+
+    NSURL *url = [NSURL URLWithString:string];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+    
+    __weak __typeof(self)weakSelf = self;
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        if ([responseObject[@"status"] intValue] != 0) {
+            [weakSelf shownUnknownErrorAlert];
+        } else {
+            [weakSelf processResponseDictionary:responseObject[@"result"]];
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Connection error"
+                                                            message:[error localizedDescription]
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+    }];
+    [operation start];
+}
+
+- (void)processResponseDictionary:(NSDictionary *)responseDict {
+
+}
+
+
+- (void)shownUnknownErrorAlert {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Unknown error"
+                                                        message:@"Please, try again later"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+    [alertView show];
 }
 
 - (void)viewDidLayoutSubviews {
